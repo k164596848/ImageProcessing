fef61951c8082ab5bb2930251c10aa1ddae5df9a
k164596848 <k164596848@gmail.com>
2020-09-24 10:15:30 +0800

    Fi

---
+++ ImageProcessing/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV.CvEnum;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ImageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_loadimage = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.imageBox4 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_loadimage
+            // 
+            this.button_loadimage.Location = new System.Drawing.Point(162, 42);
+            this.button_loadimage.Name = "button_loadimage";
+            this.button_loadimage.Size = new System.Drawing.Size(84, 31);
+            this.button_loadimage.TabIndex = 0;
+            this.button_loadimage.Text = "Load Image";
+            this.button_loadimage.UseVisualStyleBackColor = true;
+            this.button_loadimage.Click += new System.EventHandler(this.button_loadimage_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(270, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(431, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChange);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(82, 176);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(300, 300);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(459, 176);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(267, 300);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.Location = new System.Drawing.Point(785, 176);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(254, 309);
+            this.imageBox3.TabIndex = 2;
+            this.imageBox3.TabStop = false;
+            // 
+            // imageBox4
+            // 
+            this.imageBox4.Location = new System.Drawing.Point(1118, 176);
+            this.imageBox4.Name = "imageBox4";
+            this.imageBox4.Size = new System.Drawing.Size(258, 309);
+            this.imageBox4.TabIndex = 2;
+            this.imageBox4.TabStop = false;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1446, 700);
+            this.Controls.Add(this.imageBox4);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_loadimage);
+            this.Name = "Form1";
+            this.Text = "]=";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_loadimage;
+
+        private TextBox textBox1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private Emgu.CV.UI.ImageBox imageBox4;
+    }
+}
+
---
+++ ImageProcessing/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV.CvEnum;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+
+namespace ImageProcessing
+{
+    public partial class Form1 : Form
+    {      
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_loadimage_Click(object sender, EventArgs e)
+        {
+            imageBox1.Image = null;
+            imageBox2.Image = null;
+            imageBox3.Image = null;
+            imageBox4.Image = null;
+            //開啟檔案
+            OpenFileDialog filename = new OpenFileDialog();
+            if (filename.ShowDialog() == DialogResult.OK)
+            {
+                //將檔案路經存至Textbox1
+                textBox1.Text = filename.FileName;
+
+                
+            }
+        }
+
+        private void textBox1_TextChange(object seder,EventArgs e)
+        {
+            //將影像變形至400*400
+
+            //讀取彩色影像
+            Image<Bgr, byte> colorImage = new Image<Bgr, byte>(textBox1.Text);
+            imageBox1.Image = colorImage;
+
+            //取得灰階影像
+            Image<Gray, byte> grayImage = colorImage.Convert<Gray, byte>();
+            grayImage.Save(textBox1.Text+"gray.jpg" );
+            imageBox2.Image = grayImage;
+
+             
+            //二值化的閥值
+            Gray thresholdValue = new Gray(140);
+            //取得二值化影像
+            Image<Gray, byte> thresholdImage = grayImage.ThresholdBinary(thresholdValue, new Gray(255));          
+            //灰度反轉
+            //thresholdImage = 255 - thresholdImage;            
+            imageBox3.Image = thresholdImage;
+            thresholdImage.Save(textBox1.Text + "threshold.jpg");
+
+
+            //擴張處理(擴張x像素)
+            Image<Gray, byte> ErodeImage = thresholdImage.Erode(1);
+            imageBox4.Image = ErodeImage;
+            ErodeImage.Save(textBox1.Text + "Ecode.jpg");
+
+
+
+
+        }
+
+    }
+}
